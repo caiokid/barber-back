9237e7fac81a3dce54d09785de6c018c502e7aea
Caio Vinicius Franca da Silva <caioviniciusfranca@hotmail.com>
2026-04-21 21:34:35 -0300

    fix: cookie for none

--- controllers/auth.js
+++ controllers/auth.js
@@ -71,7 +71,7 @@ const login = async (req, res, next) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: 'none',
       maxAge: 24 * 60 * 60 * 1000
     });
 
